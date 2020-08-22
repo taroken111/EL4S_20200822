0324e0216753210e2d640449f636a1aaaabd9ce0
ryouhei <ryouhei05205005@gmail.com>
2020-08-22 14:04:09 +0900

    タイトルシーンの作成

---
+++ EL4S_20200822/Assets/Suzuki/Fade/Scripts/FadeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeManager : MonoBehaviour
+{
+    public Fade fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            fade.FadeIn(2.0f,"SampleScene");
+        }
+    }
+
+    public void SceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
