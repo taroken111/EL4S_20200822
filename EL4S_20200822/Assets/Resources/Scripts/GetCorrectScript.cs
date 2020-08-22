db79fa3089d550c28888b346e638e208def64832
Crocell94 <spartan202@gmail.com>
2020-08-22 13:05:00 +0900

    リザルト画面作成

---
+++ EL4S_20200822/Assets/Resources/Scripts/GetCorrectScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCorrectScript : MonoBehaviour
+{
+    public static GetCorrectScript m_Instance;
+
+    public int m_Correct;
+    void Awake()
+    {
+        //!instanceがuniqueであるように
+        if (m_Instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            m_Instance = this;
+        }
+        else if (m_Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ EL4S_20200822/Assets/Resources/Scripts/ToTitleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+public class ToTitleScript : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.UI.Button m_TitleButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //! 押したら実行する関数を設定
+        m_TitleButton.onClick.AddListener(Title);
+    }
+
+    void Title()
+    {
+        //! タイトルへ遷移
+        SceneManager.LoadScene("TitleScene");
+    }
+}
